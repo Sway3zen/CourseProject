75488d79ce5ec23f43eab9367741388ef1588755
Swayze <kovaloff10@gmail.com>
2023-05-01 23:40:32 +0300

    fd

--- CourseProject4/ViewResultPeople.h
+++ CourseProject4/ViewResultPeople.h
@@ -1,3 +1,9 @@
+#include <dirent.h>
+#include <direct.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
 #pragma once
 
 namespace CourseProject4 {
@@ -62,9 +68,100 @@ namespace CourseProject4 {
 			this->Name = L"ViewResultPeople";
 			this->ShowIcon = false;
 			this->Text = L"ViewResultPeople";
+			this->Load += gcnew System::EventHandler(this, &ViewResultPeople::ViewResultPeople_Load);
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
+
+	private:
+		int files;
+		int CheckDirectory() {
+			int g = 0;
+
+			char* temp_path = getenv("TEMP");
+			char* number_test = GetNumberTest();
+			char dir_name[255];
+			sprintf(dir_name, "%s\\Testify\\Result\\%s\\", temp_path, number_test);
+
+			struct dirent* de;
+
+			DIR* dr = opendir(dir_name);
+
+			if (dr == NULL) {
+				System::Windows::Forms::MessageBox::Show("В данний момент немає активних тестів.", "Помилка", System::Windows::Forms::MessageBoxButtons::OK, System::Windows::Forms::MessageBoxIcon::Error);
+				this->Close();
+			}
+
+			while ((de = readdir(dr)) != NULL) {
+				g++;
+			}
+
+			return files = g-4;
+		}
+
+		array<String^>^ GetNames() {
+			char* temp_path = getenv("TEMP");
+			char* folder_path_result = "\\Testify\\Result\\";
+			char* number_test = GetNumberTest();
+
+			char file_name[255];
+
+			sprintf(file_name, "%s%s%s\\Global_Result.txt", temp_path, folder_path_result, number_test);
+
+			FILE* fp = fopen(file_name, "r");
+
+			if (fp == NULL) {
+				return gcnew array<String^>(0);
+			}
+
+
+			array<String^>^ values = gcnew array<String^>(files);
+			int i = 0;
+			char line[256];
+			while (fgets(line, sizeof(line), fp)) {
+				String^ test = gcnew String(line);
+				values[i] = test->Trim();
+				String^ tested = gcnew String(values[i]);
+
+				System::Windows::Forms::MessageBox::Show(tested, "Помилка", System::Windows::Forms::MessageBoxButtons::OK, System::Windows::Forms::MessageBoxIcon::Error);
+				i++;
+			}
+
+			fclose(fp);
+			
+
+			return values;
+		}
+
+		char* GetNumberTest() {
+			char* temp_path = getenv("TEMP");
+			char test_number[255];
+			sprintf(test_number, "%s\\Testify\\Current info\\Current_test.bin", temp_path);
+
+			char number[255];
+			FILE* fp = fopen(test_number, "rb");
+
+			char line[256];
+
+			if (fp == NULL) {
+				//this->Close();
+			}
+			else {
+				while (fscanf(fp, "%s", &line) != EOF) {
+				}
+			}
+
+			fclose(fp);
+
+			char* result = (char*)malloc(strlen(line) + 1);
+			strcpy(result, line);
+			return result;
+		}
+
+	private: System::Void ViewResultPeople_Load(System::Object^ sender, System::EventArgs^ e) {
+		CheckDirectory();
+		array<String^>^ Names = GetNames();
+	}
 	};
 }
